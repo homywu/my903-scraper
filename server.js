d6a966ed97d9f374e09d083eb9bba1c8d4b9d9db
homywu <homywu@gmail.com>
2016-09-04 01:35:16 +0000

    change server.js

--- server.js
+++ server.js
@@ -3,82 +3,37 @@
 //
 // A simple chat server using Socket.IO, Express, and Async.
 //
-var http = require('http');
-var path = require('path');
-
-var async = require('async');
-var socketio = require('socket.io');
+var fs = require('fs');
 var express = require('express');
+var cheerio = require("cheerio");
+var request = require("request");
+var app = express();
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
-
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
-
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
+app.get('/scrape', function(req, res){
+  
+  var url = "www.my903.tk/page/1";
+  
+  request(url, function(error, response, html){
+    
+    if(!error){
+      var $ = cheerio.load(html);
+      
+      var mp3;
+      
+      $('.wp-audio-shortcode').each(function() {
+        var data = $(this);
+        
+        mp3 = data.first().text();
+        
+        console.log(mp3);
+      })
     }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+  })
+  
 });
+
+app.listen('8081');
+
+console.log('app started on port 8081');
+
+exports = module.exports = app;
